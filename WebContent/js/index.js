625f7a9730ed99fa8414adde87d2bba866020705
Ariel <jdecks.visible@outlook.com>
2017-03-16 18:33:47 -0600

    cambios

--- WebContent/js/index.js
+++ WebContent/js/index.js
@@ -1,13 +1,12 @@
 $(document).ready(function(){
-    var link = $("#productoCategoriesList > li");
+    
+	var link = $("#productoCategoriesList > li");
     link.on("click", function(){
         var $i = $(this).find("i");
+        $i.toggleClass("fa-plus");
+        $i.toggleClass("fa-minus").css("margin-right","10px");
+    }); 
 
-        // $i.toggleClass("fa-plus");
-        //$i.toggleClass("fa-minus").css("margin-right","10px");
-
-
-    })
     $('img')
     .wrap('<span style="display:inline-block"></span>')
     .css('display', 'block')
