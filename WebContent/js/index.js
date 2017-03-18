6daafa5b78d52b09a633255bbcaa08678ef9b2ab
Ariel <jdecks.visible@outlook.com>
2017-03-17 18:04:33 -0600

    modal animated

--- WebContent/js/index.js
+++ WebContent/js/index.js
@@ -1,18 +1,33 @@
-$(document).ready(function(){
-    
-	var link = $("#productoCategoriesList > li");
+/*This function lets change the fa-plus and fa-minus classes for the list*/
+function toggleClassProductCategoryList(){
+	var link = $(".productoCategoriesList > li");
     link.on("click", function(){
         var $i = $(this).find("i");
         $i.toggleClass("fa-plus");
         $i.toggleClass("fa-minus").css("margin-right","10px");
     }); 
-
-    $('img')
+}
+/*This function lets handle zoom for all the image which implements*/
+function handleImageZoom(){
+	$('img')
     .wrap('<span style="display:inline-block"></span>')
     .css('display', 'block')
     .parent()
     .zoom();
-    $("img.lazy").lazyload({
+    
+	$("img.lazy").lazyload({
         effect: "fadeIn"
-    });
+    });	
+}
+
+$(document).ready(function(){
+    toggleClassProductCategoryList(); 
+	handleImageZoom(); 
+
+	$(".modal").on("show.bs.modal", function(){
+		$(this).addClass('animated flipInY');
+	}); 
+	
+
+
 });
