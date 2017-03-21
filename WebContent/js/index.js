b14619157aeebbce7ef8b92f519e9333b1138a46
Ariel <jdecks.visible@outlook.com>
2017-03-20 21:43:02 -0600

    cambios

--- WebContent/js/index.js
+++ WebContent/js/index.js
@@ -5,7 +5,7 @@ function toggleClassProductCategoryList(){
         var $i = $(this).find("i");
         $i.toggleClass("fa-plus");
         $i.toggleClass("fa-minus").css("margin-right","10px");
-    }); 
+    });
 }
 /*This function lets handle zoom for all the image which implements*/
 function handleImageZoom(){
@@ -14,20 +14,33 @@ function handleImageZoom(){
     .css('display', 'block')
     .parent()
     .zoom();
-    
+
 	$("img.lazy").lazyload({
         effect: "fadeIn"
-    });	
+    });
 }
 
 $(document).ready(function(){
-    toggleClassProductCategoryList(); 
-	handleImageZoom(); 
+    toggleClassProductCategoryList();
+	handleImageZoom();
+
+    $(".items").find("li a").on("click", function(e){
+        e.preventDefault();
+        var $img = $(this).find("img");
+        console.log($img);
+        console.log($img.attr("src"));
+        $("#modal").find("img").attr("src", $img.attr("src"));
+        $('#modal').modal('show');
+
+
+
+    })
 
 	$(".modal").on("show.bs.modal", function(){
 		$(this).addClass('animated flipInY');
-	}); 
-	
+	});
+
+
 
 
 });
