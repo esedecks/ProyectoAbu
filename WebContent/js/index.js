4cea10925a768024d663509240f8eb22dd8597f7
Ariel <jdecks.visible@outlook.com>
2017-03-31 18:08:41 -0600

    cambios

--- WebContent/js/index.js
+++ WebContent/js/index.js
@@ -54,11 +54,11 @@ $(document).ready(function(){
     }, 1500)
 
     //Add Hover effect to menus
-    $('ul.nav li.dropdown').hover(function() {
-      $(this).find('.dropdown-menu').stop(true, true).slideDown();
-    }, function() {
-      $(this).find('.dropdown-menu').stop(true, true).slideUp();
-    });
+//    $('ul.nav li.dropdown').hover(function() {
+//      $(this).find('.dropdown-menu').stop(true, true).slideDown();
+//    }, function() {
+//      $(this).find('.dropdown-menu').stop(true, true).slideUp();
+//    });
 
 
 
